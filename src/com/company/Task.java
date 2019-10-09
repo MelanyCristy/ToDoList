e49e607d1e1becfa442da9a7f21e7bbd8a85eb85
Melany <melanycristyg@gmail.com>
2019-10-09 16:04:17 +0200

    Task class created

---
+++ src/com/company/Task.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+
+ class Task {
+
+     private String description;
+     private String title;
+
+     Task(String description,String title){
+         this.description=description;
+         this.title=title;
+
+     }
+
+     public String getDescription() {
+         return description;
+     }
+     public String getTitle() {
+         return title;
+     }
+
+
+ }
